9317028d23025c0aa989210f771be5714d413806
luo.wenxing <luo.wenxing@xiaoyi.com>
2017-11-15 10:43:09 +0800

    sendmail

---
+++ mysqlConn.py
@@ -0,0 +1,20 @@
+﻿import MySQLdb
+
+conn = MySQLdb.connect("10.10.7.58","xiaoyi","ApK1e1iMyqCprb8z","test" )
+cursor = conn.cursor()
+# 创建user表:
+cursor.execute('create table IF NOT EXISTS user (id varchar(20) primary key, name varchar(20))')
+# 插入一行记录，注意MySQL的占位符是%s:
+cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'Michael'])
+print cursor.rowcount
+conn.commit()
+cursor.close()
+
+# 运行查询:
+cursor = conn.cursor()
+cursor.execute('select * from user where id = %s', ('1',))
+values = cursor.fetchall()
+print values
+# 关闭Cursor和Connection:
+cursor.close()
+conn.close()
--- sendMail.py
+++ sendMail.py
@@ -1,5 +1,8 @@
-﻿from email.header import Header
+﻿from email import encoders
+from email.header import Header
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from email.utils import parseaddr, formataddr
 import smtplib
 
@@ -14,10 +17,27 @@ password = 'baobao871027'
 to_addr = '262737197@qq.com'
 smtp_server = 'smtp.126.com'
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+# msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg = MIMEMultipart()
 msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
 msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
-msg['Subject'] = Header(u'来自smtp……', 'utf-8').encode()
+msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
+msg.attach(MIMEText('send with file...', 'plain', 'utf-8'))
+
+# 添加附件就是加上一个MIMEBase，从本地读取一个图片:
+with open('C:/Users/hu.hang/Downloads/1493291444690_fw.png', 'rb') as f:
+    # 设置附件的MIME和文件名，这里是png类型:
+    mime = MIMEBase('image', 'png', filename='test.png')
+    # 加上必要的头信息:
+    mime.add_header('Content-Disposition', 'attachment', filename='test.png')
+    mime.add_header('Content-ID', '<0>')
+    mime.add_header('X-Attachment-Id', '0')
+    # 把附件的内容读进来:
+    mime.set_payload(f.read())
+    # 用Base64编码:
+    encoders.encode_base64(mime)
+    # 添加到MIMEMultipart:
+    msg.attach(mime)
 
 server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
